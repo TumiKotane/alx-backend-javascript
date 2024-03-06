191442e7536d3de0e4f149a40391bd3f747ed349
TumiKotane <kaiineoyoung@gmail.com>
2024-03-06 13:48:22 +0000

    Debug

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,4 @@
-export default function createIteratorObject(report){
+export default function createIteratorObject(report) {
 	const myarray = [];
 	for (const item of Object.values(report.allEmployees)) {
 		myarray.push(...item);
--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,5 @@
-export default function getSumOfHoods(
-	initialNumber,
-	expansion1989 = 89,
-	expansion2019 = 19,
-){
-	return initialNumber + expansion1989 + expansion2019;
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
 }
