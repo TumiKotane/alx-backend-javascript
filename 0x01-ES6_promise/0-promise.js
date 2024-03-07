19863a8792e379fd4aa12888df52441870b7c249
TumiKotane <kaiineoyoung@gmail.com>
2024-03-07 18:01:46 +0000

    Task 0 and 1

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,4 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable */
-export default function getResponseFromAPI(){
-  const promise = new Promise(function(resolve, reject) {
-  if (true) {
-    resolve("Stuff worked!");
-  }
-  else {
-    reject(Error("It broke"));
-  }
-});
-  return promise;
+export default function getResponseFromAPI() {
+  return (new Promise((resolve, reject) => {}));
 }
